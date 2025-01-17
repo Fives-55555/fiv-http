e62363964a2f109b0c4980738c6774c127fbbf04
Fives-55555 <linus.pohlmann@icloud.com>
2025-01-17 14:00:28 +0100

    Client Hello Finish TLS1.0

--- src/tls/tls1_0.rs
+++ src/tls/tls1_0.rs
@@ -21,9 +21,16 @@ fn to_handshake(buf: &[u8]) -> Result<TLSPayload, ServerError> {
     };
     let mut buf: &[u8] = &buf[4..];
     let inner = match kind {
-        TLSHandshakeType::ClientHello => {
-            let max_version: TLSVersion = match TLSVersion::cast(buf[0], buf[1]) {
-                Ok(suppored) => suppored,
+        TLSHandshakeType::ClientHello =>to_client_hello(buf),
+        _ => todo!(),
+    }?;
+    return Ok(TLSPayload::Handshake(inner));
+}
+
+fn to_client_hello(buf: &[u8])->Result<Client, ServerError> {
+    let len = buf.len();
+    let max_version: TLSVersion = match TLSVersion::cast(buf[0], buf[1]) {
+                Ok(supported) => supported,
                 Err(err) => match err {
                     ServerError::VERHIGH => MAX_VERSION,
                     ServerError::VERLOW => {
@@ -33,29 +40,28 @@ fn to_handshake(buf: &[u8]) -> Result<TLSPayload, ServerError> {
             };
             let client_random: &[u8] = &buf[2..34];
             let session_id_len: usize = buf[34] as usize;
+            if session_id_len < len - 34 {
+                return Err(ServerError::INVPAR);
+            }
             let mut index: usize = 35 + session_id_len;
             let sessino_id: Option<&[u8]> = if session_id_len != 0 {
                 Some(&buf[35..index])
             } else {
                 None
             };
-            let cipher_len: usize = u16::from_be_bytes([buf[index], buf[index + 2]]) as usize;
+            let cipher_len: usize = u16::from_be_bytes([buf[index], buf[index + 1]]) as usize;
             index += 2;
-            let cipher_list: Option<&[u8]> = if session_id_len != 0 {
-                Some(&buf[index..index + cipher_len])
-            } else {
-                None
-            };
+            if cipher_len < len-index && cipher_len % 2 == 0 {
+                return Err(ServerError::INVPAR);
+            }
+            let cipher_list: Vec<Cipher> = Cipher::from_slice(buf[index..index+cipher_len]);
             index += cipher_len;
             let compression_len: usize = buf[index] as usize;
             index += 1;
-            let compression: Vec<Compression> = if session_id_len != 0 {
-                Some(&buf[index..index + cipher_len])
-            } else {
-                None
-            };
-            index += cipher_len;
-
+            if compression_len == len-index {
+                return Err(ServerError::INVPAR)
+            }
+            let compression: Vec<Compression> = Compression::from_slice(&buf[index..);
             TLSHandshakePayload::ClientHello(Client {
                 version: max_version,
                 random: client_random,
@@ -63,10 +69,6 @@ fn to_handshake(buf: &[u8]) -> Result<TLSPayload, ServerError> {
                 ciphers: cipher_list,
                 compresion: compression,
             })
-        }
-        _ => todo!(),
-    };
-    return Ok(TLSPayload::Handshake(inner));
 }
 
 fn to_alert(buf: &[u8]) -> Result<TLSPayload, ServerError> {
